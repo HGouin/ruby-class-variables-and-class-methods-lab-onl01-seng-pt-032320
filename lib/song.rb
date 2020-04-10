5dd61a57dc2957dfef621118913e97d8d3c2b4ef
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-10 19:54:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,12 @@
 class Song
   @@count = 0
+  @@artist = 0
   attr_accessor :name, :artist, :genre
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count+=1
+    @@artist+=1
   end
 end
